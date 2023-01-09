da9f922e15999d2a80d20bc5a0e49f865387ec46
Meiby Hernández Morales <meahernandezmo@unal.edu.co>
2023-01-09 16:25:52 +0100

    channel card update

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { Box, CardContent, CardMedia, Typography, Typogrpahy } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
-const ChannelCard = () => {
-  return (
-    <div>ChannelCard</div>
-  )
+import { demoProfilePicture } from "../utils/constants";
+
+const ChannelCard = ({ channelDetail }) => {
+    <Box
+        sx={{
+            boxShadow: 'none',
+            borderRadius: '20px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: { xs: '356px', md: '320px' },
+            height: '326px',
+            margin: 'auto',
+            
+        }}>
+
+        <Link to={`/channel/${channelDetail?.id?.channelDetail}`}>
+            <CardContent sx={{
+                display: 'flex', flexDirection: 'column', justifyContent: 'center',
+                textAlign: 'center', color: '#fff'
+            }}>
+                <CardMedia
+                    image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+                    alt={channelDetail?.snippet?.title}
+                    sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }}
+                ></CardMedia>
+                <Typography variant="h6">
+                    {channelDetail?.snippet?.title}
+                    <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }}></CheckCircle>
+                </Typography>
+                {channelDetail?.statistics?.subscriberCount && (
+                    <Typography sx={{ fontSize: '15px', fontWeight: 500, color: 'gray' }}>
+                        {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString('en-US')} Subscribers
+                    </Typography>
+                )}
+            </CardContent>
+        </Link>
+
+    </Box>
 }
 
 export default ChannelCard
--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,20 +4,33 @@ import { CheckCircle } from "@mui/icons-material";
 
 import { demoThumbnailUrl, demoVideoUrl, demoChannelTitle, demoVideoTitle, demoChannelUrl } from "../utils/constants";
 
-const VideoCard = ({video: {id:{
-    videoId}, snippet}}) => {
-  return (
-    <Card>
-        <Link to={videoId ? `/video/${videoId}`: demoVideoUrl}>
-            <CardMedia 
-            image={snippet?.thumbnails?.high?.url}
-            alt={snippet?.title}
-            sx={{width: 358, height: 180}}
-            >
-            </CardMedia>
-        </Link>
-    </Card>
-  )
+const VideoCard = ({ video: { id: {
+    videoId }, snippet } }) => {
+    return (
+        <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
+            <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                <CardMedia
+                    image={snippet?.thumbnails?.high?.url}
+                    alt={snippet?.title}
+                    sx={{ width: 358, height: 180 }}
+                >
+                </CardMedia>
+            </Link>
+            <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+                <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                    <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+                        {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+                    </Typography>
+                </Link>
+                <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+                    <Typography variant="subtitle2" fontWeight="bold" color="gray">
+                        {snippet?.channelTitle || demoChannelTitle}
+                        <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }}></CheckCircle>
+                    </Typography>
+                </Link>
+            </CardContent>
+        </Card>
+    )
 }
 
 export default VideoCard
